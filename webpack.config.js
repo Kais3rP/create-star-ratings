6ce03bd0927bcc3aff1c816ac54217c065a5bc20
Kais3rP <cesare.polonara@gmail.com>
2020-09-11 16:12:32 +0200

    added terser plugin

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+              })
+            ]
+      },
+}
